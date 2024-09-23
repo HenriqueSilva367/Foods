ef2fef44a24b059a7f293aa5c3c2759f1ce99879
Henrique Silva <henrique@taller.net.br>
2024-09-23 16:44:40 -0300

    feat: add new styled product item

---
+++ app/_components/product-item.tsx
@@ -0,0 +1,61 @@
+import Image from "next/image";
+import { Prisma } from "@prisma/client";
+import { calculateProductPrice, formateCurrency } from "../_helpers/price";
+import { ArrowDownIcon } from "lucide-react";
+
+type ProductItemProps = {
+  product: Prisma.ProductGetPayload<{
+    include: {
+      restaurant: {
+        select: {
+          name: true;
+        };
+      };
+    };
+  }>;
+};
+
+export const ProductItem = ({ product }: ProductItemProps) => {
+  return (
+    <div className="space-y-2 min-w-[150px] w-[150px]">
+      <div className="h-[150px] w-full relative">
+        <Image
+          src={product.imageUrl}
+          alt={product.name}
+          fill
+          className="object-cover rounded-lg shadow-md"
+        />
+
+        {product.discountPercentage && (
+          <div className="flex gap-[2px] items-center absolute top-2 left-2 bg-primary py-[2px] px-2 rounded-full text-white">
+            <ArrowDownIcon size={12} />
+            <span className="font-semibold text-xs">
+              {product.discountPercentage}%
+            </span>
+          </div>
+        )}
+      </div>
+
+      <div>
+        <h2 className="truncate text-sm">{product.name}</h2>
+
+        <div className="flex gap-1 items-center">
+          <h3 className="font-semibold">
+            {formateCurrency(calculateProductPrice(product))}
+          </h3>
+
+          {product.discountPercentage > 0 && (
+            <span
+              className="line-through 
+              text-muted-foreground 
+              text-xs"
+            >
+              {formateCurrency(Number(product.price))}
+            </span>
+          )}
+        </div>
+        <span>{product.restaurant.name}</span>
+      </div>
+    </div>
+  );
+};
--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,9 @@ import { Category } from "./_components/category-list";
 import { Header } from "./_components/header";
 import { Search } from "./_components/search";
 import Banner from "./assets/promo-banner-01.svg";
+import { ProductList } from "./_components/product-list";
+import { Button } from "./_components/ui/button";
+import { ChevronRightIcon } from "lucide-react";
 
 export default function Home() {
   return (
@@ -16,15 +19,31 @@ export default function Home() {
         <Category />
       </div>
 
-      <Image
-        src={Banner}
-        alt="Até 30% de desconto em pizzas"
-        height={0}
-        width={0}
-        className="h-auto w-full"
-        sizes="100vw"
-        quality={100}
-      />
+      <div className="px-5 pt-6">
+        <Image
+          src={Banner}
+          alt="Até 30% de desconto em pizzas"
+          height={0}
+          width={0}
+          className="h-auto w-full object-contain"
+          sizes="100vw"
+          quality={100}
+        />
+      </div>
+
+      <div className="pt-6 space-y-4">
+        <div className="px-5 flex justify-between items-center">
+          <h2 className="font-semibold">Pedidos Recomendados</h2>
+          <Button
+            variant="ghost"
+            className=" h-fit p-0 text-primary hover:bg-transparent"
+          >
+            Ver todos
+            <ChevronRightIcon size={16} />
+          </Button>
+        </div>
+        <ProductList />
+      </div>
     </>
   );
 }
