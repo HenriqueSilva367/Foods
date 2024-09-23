922d0a37ba17d20762d9cf06f10b42dda43c85be
Henrique Silva <henrique@taller.net.br>
2024-09-23 12:03:53 -0300

    feat: add promo banner to hame page

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,8 @@
+import Image from "next/image";
 import { Category } from "./_components/category-list";
 import { Header } from "./_components/header";
 import { Search } from "./_components/search";
+import Banner from "./assets/promo-banner-01.svg";
 
 export default function Home() {
   return (
@@ -13,6 +15,16 @@ export default function Home() {
       <div className="px-5 pt-6">
         <Category />
       </div>
+
+      <Image
+        src={Banner}
+        alt="Até 30% de desconto em pizzas"
+        height={0}
+        width={0}
+        className="h-auto w-full"
+        sizes="100vw"
+        quality={100}
+      />
     </>
   );
 }
