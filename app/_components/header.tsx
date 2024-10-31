4b2689eb8dd7b1f0be6b43ebac16b9859995eaca
Henrique Silva <henrique@taller.net.br>
2024-10-31 17:51:38 -0300

    feat: add new link header link to home

--- app/_components/header.tsx
+++ app/_components/header.tsx
@@ -2,11 +2,22 @@ import Image from "next/image";
 import Logo from "../assets/icons/logo.svg";
 import { Button } from "./ui/button";
 import { MenuIcon } from "lucide-react";
+import Link from "next/link";
 
 export const Header = () => {
   return (
     <div className=" flex justify-between px-5 pt-6">
-      <Image src={Logo} alt="FSW Foods" height={30} width={100} />
+      <div className="relateve h-[30px] w-[100px]">
+        <Link href="/">
+          <Image
+            src={Logo}
+            alt="FSW Foods"
+            height={30}
+            width={100}
+            className="object-cover"
+          />
+        </Link>
+      </div>
       <Button
         size="icon"
         variant="outline"
